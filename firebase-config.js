0a2b8b7e2ddb651caffc703f6105d54eebe4dd06
IT Developer <malanhans777@gmail.com>
2026-01-28 13:56:12 +0500

    inital commit

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDF16WkCH9lT-33uPFOZU1R3o_3yqcRNzU",
+  authDomain: "eligible-6c799.firebaseapp.com",
+  projectId: "eligible-6c799",
+  storageBucket: "eligible-6c799.firebasestorage.app",
+  messagingSenderId: "721212595786",
+  appId: "1:721212595786:web:37347bf4baad9b737f10b3"
+};
+
+export default firebaseConfig;
